fa94f6b27b9f7ef28400bf3134f8a26905675556
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-12-24 19:11:41 -0300

    Entrada de dados

--- Program.cs
+++ Program.cs
@@ -4,13 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        int a = 5;
-        int b = 2;
 
-        double c = a / b;
-        double d = 5 / 2.0;
-        Console.WriteLine(c);
-        Console.WriteLine(d);
+        Console.WriteLine("Entre com sue nome: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Quantos quartos tem na sua casa? ");
+        int room = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre com o preço de um produto: ");
+        double price = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Entre seu último nome, idade e altura na mesma linha: ");
+        string[] vet = Console.ReadLine().Split(' ');
+
+
+        Console.WriteLine(name);
+        Console.WriteLine(room);
+        Console.WriteLine(price);
+        Console.WriteLine(vet[0]);
+        Console.WriteLine(vet[1]);
+        Console.WriteLine(vet[2]);
+
+
 
     }
 }

937eabed216fedddfaa35360c54193ad8cdb3e92
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-12-24 17:35:28 -0300

    Manipulando strings

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Course;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int age = 32;
+        string name = "Maria";
+        double balance = 10.2568;
+
+        // Placeholders
+        Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:f2}", name, age, balance);
+
+        //Interpolação
+        Console.WriteLine($"{name} tem {age} anos e tem saldo igual a {balance:f2} ");
+
+        //Concatenação
+        Console.WriteLine(name + " tem " + age + " anos e tem saldo igual a " + balance.ToString("F2"));
+
+    }
+}
+
